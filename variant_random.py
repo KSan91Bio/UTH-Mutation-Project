46b721b309bde333c07becaae27813fac978b9c0
KSan91Bio <43620773+KSan91Bio@users.noreply.github.com>
2018-11-09 09:32:50 -0600

    updated randomization with if statement

--- variant_random.py
+++ variant_random.py
@@ -13,15 +13,17 @@ sorted_list = []
 token_list = []
 iteration_list = ""
 
-print ('Loading matches.\n')
-b = pybedtools.BedTool('matches_chr1.txt')
-print ('Loaded ' + str(b.count()))
+# print ('Loading matches.\n')
+# b = pybedtools.BedTool('matches_chr1.txt')
+# print ('Loaded ' + str(b.count()))
 
-num_trials = 2
+num_trials = 20
 max_rand_shift = 1000
 
+file = open('cosmictrial20.bed', 'w')
+
 print ('Loading variants.\n')
-with open ("cosmictest.txt", "r") as c:
+with open ("L_cosmic.bed", "r") as c:
     cosmic = c.readlines()
     for line in cosmic:
         tokens = line.split('\t')
@@ -33,8 +35,9 @@ with open ("cosmictest.txt", "r") as c:
         
 print ('Loaded ' + str(len(cosmic)) +' variants.')
 
+#add line for opening new file, prepare for tomorrow
 for r in range(num_trials):
-    print ('Running ' + str(r+1) + ' iteration.\n')
+    #print ('Running ' + str(r+1) + ' iteration.\n')
     
     trial_list = []
     random.seed()
@@ -50,14 +53,16 @@ for r in range(num_trials):
         chromo = modify[0]
         start = int(modify[1])
         end = int(modify[2])
-        #chromo = str(line.split("\t")[0])
-        #start = int(line.split("\t")[1])
-        #end = int(line.split("\t")[2])
+
         
         start_s = ran_pos + start
         start_e = ran_pos + end
-        #randomized = randomized + "\n" + str(chromo) + "\t" + str(start_s) + "\t" + str(start_e) + "\t" + "trial: " + str(r+1)
-        randomized = str(chromo) + "\t" + str(start_s) + "\t" + str(start_e) + "\t" + "trial:" + str(r+1) + "\t"
+        
+        if start_s > 0:
+            randomized = str(chromo) + "\t" + str(start_s) + "\t" + str(start_e) + "\t" + "trial:" + str(r+1) + "\n"
+        #iteration_list = iteration_list + randomized
+        #if num_trials == 2:
+            #randomized = str(chromo) + "\t" + str(start_s) + "\t" + str(start_e) + "\t" + "trial:" + str(r+1)
         iteration_list = iteration_list + randomized
         #randomized_combined += int(randomized[modify])
 #         if n_line % 1000 == 0:
@@ -66,17 +71,21 @@ for r in range(num_trials):
         
 #print (randomized)
 #print (iteration_list)
+file.write(iteration_list)
+print ("program complete")
+file.close()
+
 #print (randomized_combined)
-print ('Creating bedtool object from randomized regions.\n')
-RanIntersect = BedTool(iteration_list, from_string = True)
-#print (RanIntersect)
-print ('Creating pybedtools object from the regions.')
-a = pybedtools.BedTool(RanIntersect)
-#print (a)
-# print ('Created ' + str(a.count()) + ' regions.')
+# print ('Creating bedtool object from randomized regions.\n')
+# RanIntersect = BedTool(iteration_list, from_string = True)
+# #print (RanIntersect)
+# print ('Creating pybedtools object from the regions.')
+# a = pybedtools.BedTool(RanIntersect)
+# #print (a)
+# # print ('Created ' + str(a.count()) + ' regions.')
 
-print ('Intersecting regions.\n')
-Intersecttrial = (b.intersect(a, u=True, stream=True))
+# print ('Intersecting regions.\n')
+# Intersecttrial = (b.intersect(a, u=True, stream=True))
 
-# print ('Processing intersections.\n')
-print (Intersecttrial)
+# # print ('Processing intersections.\n')
+# print (Intersecttrial)
